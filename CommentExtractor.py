e586506d7a912be1a55b2473658f91e933fc2718
mitchboulton <45919646+mitchboulton@users.noreply.github.com>
2019-01-01 19:25:19 -0500

    Now imports a ticker list from external .txt document

--- CommentExtractor.py
+++ CommentExtractor.py
@@ -5,10 +5,6 @@ from collections import Counter
 def BMP(s):
     return "".join((i if ord(i) < 10000 else '\ufffd' for i in s))
 
-# Finds whole word - not used currently, will need to be deleted later if not used
-# def findWholeWord(w):
-#    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-
 # Pulls Reddit credentials from locally stored file
 raw_credentials = open('Reddit_Creds.txt')
 listed_credentials = []
@@ -24,7 +20,7 @@ reddit = praw.Reddit(client_id=listed_credentials[0],
 # print(reddit.read_only)
 
 # Selects the reddit thread - this is a short one as a test
-submission = reddit.submission(id='ab5h45')
+submission = reddit.submission(id='ablzh6')
 
 # Prints number of comments in thread (reference for now - to be deleted_
 print(submission.num_comments) 
@@ -34,29 +30,35 @@ print(submission.num_comments)
 # for top_level_comment in submission.comments:
 #    print(BMP(top_level_comment.body))
 
-# Creates a dictionary for tickers - will need to be developed further
-tickers = ['SPX','VIX']
-tickerCount = [0,0]
+# Ticker dictionary - creates a ticker dictionary to be counted, pulling from a txt file.
+raw_tickers = open('tickers.txt')
+tickers = []
+for string in raw_tickers:
+    tickers.append(string.rstrip())
+
+# Ticker counter - creates a list to store the count for each of the items in the ticker dictionary
+# Stores the value of zero in each item of the list.
 number_of_tickers = len(tickers)
+tickerCount = []
+j = 0
+while j < len(tickers):
+    tickerCount.append(0)
+    j = j +1 
 
-# Until the dictionary is completed and integrated, using hardcoded SPX to test.
-SPX = 0
-word = 'SPX'
-
-# Prints all comments in thread
+# Extracts all the comments out of the thread using Reddit API
 submission.comments.replace_more(limit=None)
+
+# Starts a loop to extract each comment one by one from the Reddit API
 for comment in submission.comments.list():
+
     i = 0 # Resets i to zero at the start of every comment read
     print(BMP(comment.body)) # For testing, to be deleted
-    # Searches for SPX in the comment body, needs to be hooked into a dictionary. Next step to be completed.
-    print('Number of tickers: ',number_of_tickers)
-    while i < number_of_tickers:
+
+    while i < number_of_tickers: # Loop through each item in the ticker dictionary
         if tickers[i] in comment.body:
             tickerCount[i] = tickerCount[i] + 1
-            print('----------------------------------------------')
         i = i + 1
-        print('Current value of i: ',i)
-    print('Current value of i: ',i)
-    print(tickerCount)
+
+    print(tickerCount) # To be deleted
 
 

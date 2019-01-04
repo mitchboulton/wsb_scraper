31cbd079924ffee19833a405f8159efcec3811a3
mitchboulton <45919646+mitchboulton@users.noreply.github.com>
2019-01-03 21:53:23 -0500

    Add files via upload
    Main improvements include:
     - Output is now dynamic and can analyse and display multiple threads at once
     - Formatting has been improved (i.e. segregation of functions)

--- CommentExtractor.py
+++ CommentExtractor.py
@@ -1,65 +1,133 @@
-import praw, re, csv
+import praw, re, csv, os
+from operator import add
 
 # Removes emojis from IDLE output
 def BMP(s):
     return "".join((i if ord(i) < 10000 else '\ufffd' for i in s))
 
-# Pulls Reddit credentials from locally stored file
-print('Logging into Reddit...') # To be deleted.
-raw_credentials = open('Reddit_Creds.txt')
-listed_credentials = []
-for string in raw_credentials:
-    listed_credentials.append(string.rstrip())
+# Acesses reddit API
+def reddit_creds():
+    print('Logging into Reddit...') # To be deleted.
+    raw_credentials = open('Reddit_Creds.txt')
+    listed_credentials = []
+    for string in raw_credentials:
+        listed_credentials.append(string.rstrip())
+        
+    # Credentials to access Reddit API
+    reddit = praw.Reddit(client_id=listed_credentials[0],
+                         client_secret=listed_credentials[1],
+                         user_agent=listed_credentials[2])
+    print('Logged in')
+    return reddit
 
-# Credentials to access Reddit API
-reddit = praw.Reddit(client_id=listed_credentials[0],
-                     client_secret=listed_credentials[1],
-                     user_agent=listed_credentials[2])
+def write_to_CSV(matrix):
+    outputFile = open('output.csv', 'w', newline='')
+    outputWriter = csv.writer(outputFile)
+    outputWriter.writerows(matrix) 
+    outputFile.close()
 
-# Selects the reddit thread - this is a short one as a test
-submission = reddit.submission(id='ablzh6')
-print('Done') # To be deleted.
+# Searches through WSB threads, finds WAYMT threads, returns submission ids
+def search_threads():
+    thread_limit = 300
+    phrase = 'What Are Your Moves Tomorrow'
+    thread_counter = 0
+    thread_ids,thread_date = [],[]
 
-# Ticker dictionary - creates a ticker dictionary to be counted, pulling from a CSV file.
-print('Selecting tickers from dictionary...') # To be deleted.
-raw_tickers = open('tickers.csv')
-tickers_reader = csv.reader(raw_tickers)
-tickers = list(tickers_reader)
-
-# Ticker counter - creates a list to store the count for each of the items in the ticker dictionary
-# Stores the value of zero in each item of the list.
-number_of_tickers = len(tickers)
-tickerCount = []
-j = 0
-while j < len(tickers):
-    tickerCount.append(0)
-    j = j +1 
-print('Done') # To be deleted.
-
-# Extracts all the comments out of the thread using Reddit API
-print('Extracting comments from thread...') # To be deleted.
-submission.comments.replace_more(limit=None)
-
-# Starts a loop to extract each comment one by one from the Reddit API
-for comment in submission.comments.list():
-
-    i = 0 # Resets i to zero at the start of every comment read
-    # print(BMP(comment.body)) # For testing, to be deleted
+    subreddit = reddit.subreddit('wallstreetbets')
+    for submission in subreddit.hot(limit=thread_limit):
+         
+        if phrase in submission.title:
+            thread_ids.append(submission.id)
+            thread_date.append(submission.created_utc)
+            thread_counter = thread_counter + 1
+            print(thread_counter," thread(s) found") # To be deleted
+            print(BMP(submission.title)) # To be deleted
     
-    # Starts a loop through each item in the ticker dictionary
-    while i < number_of_tickers: 
-        if tickers[i][0] in comment.body:
-            tickerCount[i] = tickerCount[i] + 1
+    return(thread_ids)
+
+def initialise_matrix(number_of_threads,
+                      number_of_tickers,
+                      row_buffer,
+                      column_buffer):
+
+    # Initialisation of blank matrix (filled with zeros)
+    
+    matrix = [[0 for x in range(number_of_threads+column_buffer)]
+              for y in range(number_of_tickers+row_buffer)]
+
+    matrix[0][0] = ""
+
+    # Changes the top row of the matrix
+    i = 0 
+    while i < number_of_threads:
+        matrix[0][i+column_buffer] = "ID " + str(i+1)
         i = i + 1
 
-    # print(tickerCount) # To be deleted
-print('Done') # To be deleted.
+    # Changes left column of the matrix
+    i = 0
+    while i < number_of_tickers:
+        matrix[i+row_buffer][0] = tickers[i]
+        i = i + 1
+    
+    return matrix
+    return number_of_tickers
+    return number_of_threads
 
-# Writes both lists to a CSV file.
-print('Writing to CSV file...') # To be deleted.
-outputFile = open('output.csv', 'w', newline='')
-outputWriter = csv.writer(outputFile)
-outputWriter.writerows(zip(tickers,tickerCount)) # Need to figure out how to remove quotes
-outputFile.close()
-print('Done') # To be deleted.
-print('------------END--------------') # To be deleted.
+# ------------------- START MAIN --------------------------------
+
+row_buffer,column_buffer = 1,1 # Need to put this somewhere
+
+# Pulls reddit credentials from local folder and accesses API
+reddit = reddit_creds()
+
+# Selects the reddit thread - this is a short one as a test
+thread_id = search_threads()
+number_of_threads = len(thread_id) # Calculates number of threads
+print('Number of threads identified: ',number_of_threads)
+
+# Ticker dictionary - creates a ticker dictionary to be counted, pulling from a CSV file.
+raw_tickers = open('tickers.csv')
+tickers_reader = csv.reader(raw_tickers, delimiter="\t")
+tickers = list(tickers_reader)
+tickers = [row[0] for row in tickers] # Compresses the CSV input file into one list (instead of a list of lists)
+number_of_tickers = len(tickers)
+
+matrix = initialise_matrix(number_of_threads,number_of_tickers,row_buffer,column_buffer)
+print('Number of tickers: ',number_of_tickers)
+print('Number of threads: ',number_of_threads)
+
+# START OF THEAD LOOP
+
+thread_counter = 1
+
+while thread_counter < number_of_threads + 1:
+    submission = reddit.submission(id=thread_id[thread_counter - 1])
+    print('Extracting comments from thread number ',thread_counter) # To be deleted.
+    submission.comments.replace_more(limit=None)
+
+    comment_loop_counter = 0 # To be deleted
+
+    # Extracts title date and places at top of column
+    title = submission.title
+    title_date = title.split(', ')[1]
+    matrix[0][thread_counter] = title_date
+
+    for comment in submission.comments.list():
+        i = 0
+        while i < number_of_tickers: # Loops through the tickers
+            if tickers[i] in comment.body:
+                matrix[i+row_buffer][0+thread_counter] = matrix[i+row_buffer][0+thread_counter] + 1
+            i = i + 1
+
+        comment_loop_counter = comment_loop_counter + 1 # To be deleted
+        print('Analying comment number ',comment_loop_counter,' in thread number ',thread_counter) # To be deleted
+
+    thread_counter = thread_counter + 1
+
+print(matrix)
+
+# END OF THREAD LOOP
+
+# Writes the two lists to a CSV file
+write_to_CSV(matrix)
+os.startfile("output.csv")
